750a6253824d8205e3c511147f40653828d24874
Martin Dezsi <dezsimartin@gmail.com>
2019-06-18 09:38:26 +0200

    refr

--- 06.18/readline.js
+++ 06.18/readline.js
@@ -1,1 +1,22 @@
-let readline = require('readline-sync');
+/* let readline = require('readline-sync');
+let name = readline.question('What\'s your name?');
+console.log('Hello ', name, '!');
+
+while (true) {
+  let key = readline.keyIn('Press a key, \'q\' to exit!');
+  console.log('You pressed', key);
+  if (key === 'q') {
+    break;
+  }
+}
+*/
+let stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.resume();
+stdin.setEncoding('utf-8');
+stdin.on('data', (key) => {
+  console.log('You pressed', key);
+  if (key === 'q') {
+    process.exit();
+  }
+});
